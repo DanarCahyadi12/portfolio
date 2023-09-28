9d321523976479c7927389e91d6cd4ebece5a195
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-09-28 15:42:28 +0800

    Fix : Fixing metadata icon code

--- app/layout.js
+++ app/layout.js
@@ -6,15 +6,15 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Portofolio',
   description: 'My portofolio ',
+  icons : {
+    icon  : "/title.svg"
+  }
 }
  
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" style={{scrollBehavior : 'smooth'}}>
-      <head>
-      <link rel="icon" href="/title.svg" sizes="any" />
-      </head>
       <body className={inter.className}>
         <Providers>
           {children}
