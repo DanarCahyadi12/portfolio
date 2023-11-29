d0082773328ab02deb81b2b156111c8e5dd91d80
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-11-29 10:33:05 +0800

    responsive modal

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export function ModalComponent({data, action}) {
     return (
-        <Modal isOpen={action.isOpen} onClose={action.onClose} >
+        <Modal isOpen={action.isOpen} onClose={action.onClose} size={{md : '2xl' ,base: 'xs'}}>
         <ModalOverlay />
         <ModalContent padding={3}>
           <ModalHeader>
@@ -15,14 +15,14 @@ export function ModalComponent({data, action}) {
           <ModalBody pb={6}>
             <Image style={{margin: "auto"}} src={data.image} alt="project image" width={600} height={600}/>
             <Stack gap={8} marginTop={10}>
-              <Heading fontSize={'2xl'}>{data.title}</Heading>
+              <Heading fontSize={{md: '2xl', base: 'xl'}}>{data.title}</Heading>
               <Box>
-                <Text fontWeight={'medium'} fontSize={'xl'}>Built using :</Text>
-                <Text>{data.tech}</Text>
+                <Text fontWeight={'medium'} fontSize={{md: 'xl', base: 'md'}}>Built using :</Text>
+                <Text fontSize={{md: 'xl', base: 'sm'}}>{data.tech}</Text>
               </Box>
               <Box>
-                <Text fontWeight={'medium'} fontSize={'xl'}>Description:</Text>
-                <Text textAlign={'justify'}>{data.description}</Text>
+                <Text fontWeight={'medium'} fontSize={{md: 'xl', base: 'md'}}>Description:</Text>
+                <Text textAlign={'justify'} fontSize={{md: 'xl', base: 'sm'}}>{data.description}</Text>
               </Box>
             </Stack>
           </ModalBody>
